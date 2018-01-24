c84e7e86c4bfec34cdfacf76ba4d9304ff28b477
hardydrachmann <hardydrachmann@phoebuscartel.dk>
2018-01-24 10:45:20 +0100

    init

---
+++ ThorvaldLogin/Config.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+
+namespace ThorvaldLogin
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api", "My API")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedScopes = {"api"}
+                }
+            };
+        }
+    }
+}
